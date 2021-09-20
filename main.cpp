d0ec42a4e448241a7edf3b4b2a98a0f85dd8660e
Evantopian <46168209+ThisGuyEvan@users.noreply.github.com>
2021-09-20 12:17:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,13 +30,15 @@ int main(){
   std::cout << "\n\nTask C:";
   std::string date;
   date = "09/13/2018";
-  std::cout << date << ": " << compare_basin(date);
+  std::cout << "\n" << date << ": " << compare_basin(date);
   date = "09/30/2018";
-  std::cout << date << ": " << compare_basin(date);
+  std::cout << "\n" << date << ": " << compare_basin(date);
   date = "06/13/2018";
-  std::cout << date << ": " << compare_basin(date);
+  std::cout << "\n" << date << ": " << compare_basin(date);
 
   std::cout << "\n\nTask D:";
+  reverse_order("04/15/2018", "04/20/2018");
+  reverse_order("04/30/2018", "05/06/2018");
   reverse_order("05/29/2018", "06/02/2018");
 
   std::cout << std::endl;
