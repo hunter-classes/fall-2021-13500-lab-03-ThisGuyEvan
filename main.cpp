8e95eb7cdc2773beb30ab76c5c7db61a7e288527
Evantopian <46168209+ThisGuyEvan@users.noreply.github.com>
2021-09-20 12:11:10 -0400

    Updated no keyboard input.

--- main.cpp
+++ main.cpp
@@ -16,28 +16,29 @@ Assignment: Lab_3
 
 int main(){
 
-  std::cout<<"\nTask A:\nEnter a date: ";
-  std::string date;
-  std::cin >> date;
-  std::cout<< "\nEast Basin Storage: " << get_east_storage(date) << " billion gallons.";
-
+  std::cout<<"\nTask A:";
+  std::cout<< "\nEast Basin Storage: " << get_east_storage("01/20/2018") << " billion gallons.";
+  std::cout<< "\nEast Basin Storage: " << get_east_storage("04/22/2018") << " billion gallons.";
+  std::cout<< "\nEast Basin Storage: " << get_east_storage("07/19/2018") << " billion gallons.";
+  
 
   std::cout << "\n\nTask B:\n";
   std::cout << "\nminimum storage in East basin: " << get_min_east() << " billion gallons";
   std::cout << "\nMaximum storage in East basin: " << get_max_east() << " billion gallons";
 
 
-  std::cout << "\n\nTask C:\nEnter a date to compare basins: ";
-  std::cin >> date;
+  std::cout << "\n\nTask C:";
+  date = "09/13/2018";
+  std::cout << date << ": " << compare_basin(date);
+  date = "09/30/2018";
+  std::cout << date << ": " << compare_basin(date);
+  date = "06/13/2018";
   std::cout << date << ": " << compare_basin(date);
 
-
-  std::cout << "\n\nTask D:\nEnter earlier date: ";
-  std::string date1, date2;
-  std::cin >> date1;
-  std::cout << "Enter later date: ";
-  std::cin >> date2;
-  reverse_order(date1, date2);
+  std::cout << "\n\nTask D:";
+  reverse_order("05/29/2018", "06/02/2018");
+  reverse_order("11/29/2018", "12/01/2018");
+  reverse_order("12/09/2018", "12/28/2018");
 
   std::cout << std::endl;
   return 0;
